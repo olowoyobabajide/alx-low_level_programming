866e9427ecf0a7323c0149202bdf518a68676747
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-16 16:55:22 -0800

    print size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,20 @@
 /**
  * main - Entry point
  *
- * Return: Always (0) Success
+ *Return: Always (0) Success
  */
-
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", size of (char));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-        printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %zu byte(s)\n", sizeof(float));
-                return (0);
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
