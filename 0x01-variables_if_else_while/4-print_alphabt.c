6841d6ddce014480ac0db02f5a7d294b3286c1f9
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-17 18:17:28 -0800

    skip any instance of e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+			if (x == 'e')
+			{
+				continue
+			}
+			if (x == 'q')
+			{
+				continue
+			}
+	}
+	putchar('\n');
+	return (0);
+}
