540a9e88209f62a686059051e423a6676fd572be
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-17 18:05:51 -0800

    upper and lower case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x, y;
+
+	x = 'a';
+	y = 'A';
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
