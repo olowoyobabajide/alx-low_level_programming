907e50778097963c3593dce15352dde09fe8e10a
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-21 19:44:53 -0800

    ignore lowercase leters

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check the code.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
-	char r;
+    int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);	
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
