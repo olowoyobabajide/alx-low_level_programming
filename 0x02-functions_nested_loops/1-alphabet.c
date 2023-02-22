6441ba79ae5c923cade31704a77c3db20e70d83a
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-21 17:34:02 -0800

    prints alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - entry point
  *
- * Return: Always 0 (Success)
+ * Return 0 (Success)
  */
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
