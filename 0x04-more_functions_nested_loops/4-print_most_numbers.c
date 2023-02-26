25600f5ebf9ec3bf6c74cb0198c4f67d78cd7129
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-25 16:38:38 -0800

    do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0-9 except 2 & 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+	
+	while (c < 10)
+	{
+		_putchar(c + '0');
+		c++;
+
+	if (c == 2 && c == 4)
+        {
+                continue;
+        }
+
+	}
+}
