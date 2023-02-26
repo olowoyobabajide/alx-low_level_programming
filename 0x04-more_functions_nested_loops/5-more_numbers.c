65f33e188bea10b88ebd1d2d70b3ac7d3e7fd587
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-02-25 17:17:16 -0800

    print 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int a = 0, b;
+
+	while (a < 10)
+	{
+		b = 0;
+
+		while (b <= 14)
+		{
+			_putchar(b + '0');
+			b++;
+		}
+		_putchar('\n');
+	}
+}
