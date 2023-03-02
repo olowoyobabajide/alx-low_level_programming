bf739015799514f0f9126b7fb7dcbb917b20f5bb
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-03-02 13:25:53 -0800

    concatenate strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int x = 0;
-
-	n = 0;
+	int y = 0;
 
 	while (dest[x] != '\0')
 	{
 		x++;
 	}
-	while (src[n] != '\0')
+	while (y < n && src[n] != '\0')
 	{
 		dest[x] = src[n];
 		x++;
-		n++;
+		y++;
 	}
 	dest[x] = '\0';
 	return (dest);
