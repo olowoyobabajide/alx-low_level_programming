5bbd164f8332dbe2274a2fa16b3f07740657701d
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-03-02 03:31:41 -0800

    checks for alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,12 @@
  * @c: is the char to be checked
  * Return: 1 if the char is letter, 0 if otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	return ((c == 'a' && c == 'z') || (c == 'A' && c == 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
