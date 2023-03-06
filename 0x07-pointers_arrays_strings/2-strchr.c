05622acc39c52d76750b89276da1056effbec3ee
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-03-06 07:55:42 -0800

    locates a string character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strchr - entry point
+ * @s: pointer
+ * @c: character to be checked
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; a++;)
+		s[a] = c;
+	return (s);
+}
