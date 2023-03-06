4e5b338d72274f51a4803302f149852bc625e4af
Babajide Olowoyo <olowoyobabajide@gmail.com>
2023-03-06 11:38:27 -0800

    locates a char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,12 @@ char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; s[a] >= '\0'; a++)
 	{
 		if (s[a] == c)
 		{
-			return (s);
-		}
-		else
-		{
-			return (0);
+			return (s + a);
 		}
 	}
-	return (0);
+	return ('\0');
 }
