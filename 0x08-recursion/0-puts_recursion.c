740af7dace8ca858866e0a02fd4ea05a1170ad20
olowoyobabajide <olowoyobabajide@gmail.com>
2023-06-07 05:55:06 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,15 @@
  * @s: the string
  * Return: Always 0
  */
-void _puts_recursion(char *s) 
+void _puts_recursion(char *s)
 {
 	if (!*s)
 	{
 		_putchar('\n');
 	}
-	else 
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	
 }
