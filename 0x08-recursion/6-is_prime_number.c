9d12190f8e5892f122f5e31f61f97873e7cc48f4
olowoyobabajide <olowoyobabajide@gmail.com>
2023-06-07 14:34:57 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,23 +5,26 @@
  * @n: number to be checked
  * Return: 1 if prime, 0 if not prime, -1 if n < 2
  */
-int is_prime_number(int n)
-{
-    int divisor;
+int is_prime_recursive(int n, int divisor);
 
+int is_prime_recursive(int n, int divisor)
+{
     if (n < 2)
     {
         return 0;
     }
-
-    for (divisor = 2; divisor * divisor <= n; divisor++)
+    if (divisor * divisor > n)
     {
-        if (n % divisor == 0)
-        {
-            return 0;
-        }
+        return 1;
     }
-
-    return 1;
+    if (n % divisor == 0)
+    {
+        return 0;
+    }
+    return is_prime_recursive(n, divisor + 1);
 }
 
+int is_prime_number(int n)
+{
+    return is_prime_recursive(n, 2);
+}
