c2633d11775e6d0c8e49d4fdf88d49e05ba17c4c
olowoyobabajide <olowoyobabajide@gmail.com>
2023-06-14 11:49:40 -0700

    2 dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,31 +1,42 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int **alloc_grid(int width, int height) {
-    if (width <= 0 || height <= 0) {
-        return NULL;
-    }
+/**
+ * alloc_grid - allocate 2 dimensuonal grid
+ * @width: width
+ * @height: height
+ * Return: Always 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **alloc;
+	int a, b;
 
-    int **grid = (int **)malloc(height * sizeof(int *));
-    int i, j;
-    if (grid == NULL) {
-        return NULL;
-    }
-
-    for (i = 0; i < height; i++) {
-        grid[i] = (int *)malloc(width * sizeof(int));
-        if (grid[i] == NULL) {
-            for (j = 0; j < i; j++) {
-                free(grid[j]);
-            }
-            free(grid);
-            return NULL;
-        }
-
-        for (int j = 0; j < width; j++) {
-            grid[i][j] = 0;
-        }
-    }
-
-    return grid;
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	alloc = (int **)malloc(height * sizeof(int *));
+	if (alloc == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		alloc[a] = (int *)malloc(width * sizeof(int));
+		if (alloc[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+			{
+				free(alloc[b]);
+			}
+			free(alloc);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+		{
+			alloc[a][b] = 0;
+		}
+	}
+	return (alloc);
 }
-
