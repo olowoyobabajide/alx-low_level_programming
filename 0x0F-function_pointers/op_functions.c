4f5065d8e5e5df7932e82fdeb092031ce9194295
olowoyobabajide <olowoyobabajide@gmail.com>
2023-06-21 16:39:05 -0700

    TBC

---
+++ 0x0F-function_pointers/op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "calc.h"
+
+/**
+ * op_add - adds numbers
+ * @a: first digit
+ * @b: 2nd digits
+ * Return: Always 0
+ */
+int op_add(int a, int b)
+{
+	a + b;
+}
+/**
+ * op_sub - subtracts numbers
+ * @a: first digit
+ * @b: 2nd digits
+ * Return: Always 0
+ */
+int op_sub(int a, int b)
+{
+	a - b;
+}
+/**
+ * op_mul - multiplies numbers
+ * @a: first digit
+ * @b: 2nd digits
+ * Return: Always 0
+ */
+int op_mul(int a, int b)
+{
+	a * b;
+}
+/**
+ * op_div - divides numbers
+ * @a: first digit
+ * @b: 2nd digits
+ * Return: Always 0
+ */
+int op_div(int a, int b)
+{
+	a / b;
+}
+/**
+ * op_mod - gives the remainder of a division
+ * @a: first digit
+ * @b: 2nd digits
+ * Return: Always 0
+ */
+int op_mod(int a, int b)
+{
+	a % b;
+}
